16a433c7cbc7caaad2dc9a28d97db72a343b54fd
David Long <braveesnow@outlook.com>
2023-04-04 00:12:18 -0400

    Infinity ingot

--- src/main/java/org/borgor/recraft/recraftexperience/common/ItemRegistry.java
+++ src/main/java/org/borgor/recraft/recraftexperience/common/ItemRegistry.java
@@ -6,12 +6,16 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.borgor.recraft.recraftexperience.ReCraftExperience;
+import org.borgor.recraft.recraftexperience.misc.CreativeTabRegistry;
 
 import java.util.function.Supplier;
 
 public class ItemRegistry {
     private static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ReCraftExperience.MODID);
+
+    public static final RegistryObject<Item> INFINITY_INGOT = registerItem("infinity_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeTabRegistry.RECRAFT_EXPERIENCE_TAB)));
 
     public static <T extends Item> RegistryObject<T> registerItem(String name, Supplier<T> itemSupplier) {
         return ITEMS.register(name, itemSupplier);
